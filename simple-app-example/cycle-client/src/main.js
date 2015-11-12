5c6f16356639c97d1b18844b7ca7a19eb8ce3852
Joran Stark <joran.stark@comhem.se>
2015-11-12 23:10:55 +0100

    Restruct response handler and adding DELETE

--- simple-app-example/cycle-client/src/main.js
+++ simple-app-example/cycle-client/src/main.js
@@ -4,26 +4,23 @@ import {makeHTTPDriver} from '@cycle/http';
 
 function main(responses){
     const USERS_URL = "http://localhost:3000/users/userlist"
-    const USERS_ADD_URL = "http://localhost:3000/users/adduser"
+    const USER_ADD_URL = "http://localhost:3000/users/adduser"
+    const USER_DELETE_URL = "http://localhost:3000/users/deleteuser"
 
     // -- Intent --
-    let getUser$ = Cycle.Rx.Observable.just("")
-        .map(() => {
-            return {
-                url: USERS_URL,
-                method: "GET"
-            }
-        });
-
-    let showUser$ = responses.DOM.select('.linkshowuser').events('click')
+    let selectUser$ = responses.DOM.select('.linkshowuser').events('click')
         .map(ev => ev.target.rel)
         .startWith(null)
+
+    let removeUser$ = responses.DOM.select('a.linkdeleteuser').events('click')
+        .map(ev => ev.target.rel)
+        .startWith(null);
 
     let addUser$ = responses.DOM.select('button.btnAddUser').events('click')
         .map(ev => true)
         .startWith(false);
 
-    let userChanges = [
+    let userInputChange$ = [
         responses.DOM.select('input.inputUserName').events('input')
             .map(ev => ev.target.value)
             .startWith(''),
@@ -44,94 +41,81 @@ function main(responses){
             .startWith('')
         ];
 
+        let addNewUser$ = addUser$.withLatestFrom(userInputChange$,
+            function(click, username, email, fullname,age,location,gender){
+                return {username, email, fullname,age,location,gender}
+            }
+        )
 
     // -- Model --
-    let addUserModel$ = addUser$.withLatestFrom(userChanges,
-        function(click, username, email, fullname,age,location,gender){
-            return {username, email, fullname,age,location,gender}
-        }
-    )
+    const Operation = {
+      addUser: newUser => state => ({
+        user$: state.user$.concat(newUser),
+        selectedUser$: state.selectedUser$
+      }),
+      removeUser: userNameToRemove => state => ({
+        user$: state.user$.filter(user => user.userName !== userNameToRemove),
+        selectedUser$: state.selectedUser$
+      }),
+      setAllUsers: users => state => ({
+        user$: users,
+        selectedUser$: state.selectedUser$
+      }),
+      setSelectedUser: selectedUsername => state => ({
+        user$: state.user$,
+        selectedUser$: state.user$.filter(user => user.username === selectedUsername)
+      }),
+      initialState: {
+        user$: [],
+        selectedUser$: []
+      }
+    };
 
-    let receivedUser$ = responses.HTTP
-        .tap(resp => console.log("RESPONSE 1", JSON.stringify(resp)))
+    let receivedGetUsers$ = responses.HTTP
         .filter(res$ => res$.request.url.indexOf(USERS_URL) === 0)
         .mergeAll()
         .map(res => res.body)
         .startWith([])
-        .tap(resp => console.log("RESPONSE", JSON.stringify(resp)));
 
-    let usersModel$ = receivedUser$
-        .flatMap(Cycle.Rx.Observable.fromArray)
-        .merge(addUserModel$.filter(u => u.username.length > 0 ))
-        .map(u => function(oldList){
-            return oldList.concat([u])
-        })
+    let receivedAddUser$ = responses.HTTP
+        .filter(res$ => res$.request.url.indexOf(USER_ADD_URL) === 0)
+        .mergeAll()
+        .map(res => res.body)
         .startWith([])
-        .scan(function(acc, mod){
-            return mod(acc);
-        })
-        .tap(x => console.log("usersModel", JSON.stringify(x)))
 
-    let selectedUserModel$ =
-        showUser$.withLatestFrom([usersModel$],
-            function(selUserId, users){
-                let selUsers = users.filter(u => u.username === selUserId);
-                let selUser = {};
-                if(selUsers.length > 0){
-                    selUser = selUsers[0];
-                }
-                return selUser;
-            }
-        )
-        .tap(x => console.log("selectedUserModel tap", JSON.stringify(x)));
+    let receivedDeleteUser$ = responses.HTTP
+        .filter(res$ => res$.request.url.indexOf(USER_DELETE_URL) === 0)
+        .mergeAll()
+        .map(res => res.body)
+        .startWith([]);
 
-    let state = {
-        selectedUser$: selectedUserModel$,
-        users$: usersModel$
-    }
+
+    let setAllUsersOperation$ = receivedGetUsers$
+      .merge(receivedAddUser$)
+      .merge(receivedDeleteUser$)
+      .map(users => Operation.setAllUsers(users));
+
+    let selectedUserOperation$ = selectUser$.map(username => Operation.setSelectedUser(username))
+
+    let allOperations$ = Cycle.Rx.Observable.merge(
+      setAllUsersOperation$,
+      selectedUserOperation$
+    )
+
+    let state$ = allOperations$
+      .scan((state, operation) => operation(state), Operation.initialState)
 
     // -- View --
-    // addUserModel$.subscribe(function(x){
-    //     let clearValueOnElement = function(selector){
-    //         let elemUsername = document.querySelector(selector);
-    //         if(elemUsername){
-    //                 elemUsername.value="";
-    //         }
-    //     };
-    //     console.log("addUserModel subscribe", x);
-    //     [
-    //         "input.inputUserName",
-    //         "input.inputUserEmail",
-    //         "input.inputUserFullname",
-    //         "input.inputUserAge",
-    //         "input.inputUserLocation",
-    //         "input.inputUserGender"
-    //     ].forEach(clearValueOnElement)
-    //
-    // });
 
-    let inputSelectors =
-    [
-        "input.inputUserName",
-        "input.inputUserEmail",
-        "input.inputUserFullname",
-        "input.inputUserAge",
-        "input.inputUserLocation",
-        "input.inputUserGender"
-    ];
-
-    // let inputSelector$ = Cycle.Rx.Observable.just(inputSelectors);
-    // addUserModel$.combineLatest(inputSelector$).subscribe(x => console.log("addUserModel$.subscribe", x))
-
-    let vtree$ = Cycle.Rx.Observable.just([]).map(x =>
+    let vtree$ = state$.map(state =>
         h('div', [
-            h('h1', 'Cycle.js app client'),
-            h('p', 'Welcome to our test'),
+            h('h1#main-header', 'Cycle.js app client'),
+            h('p#main-text', 'Welcome to our test'),
             h('div#wrapper', [
                 h('div#userInfo', [
-                    h('h2', 'User Info'),
-                    h('p', state.selectedUser$.map(u =>
-                        h('div',[
+                    h('h2#userInfo-header', 'User Info'),
+                    h('p#selectedUser-block', state.selectedUser$.map(u =>
+                        h('div#selectedUser-container',[
                             h('strong', 'Name: '),
                             h('span#userInfoName', u.username),
                             h('br'),
@@ -148,7 +132,6 @@ function main(responses){
                 ]), // div#userInfo
                 h('div#userList', [
                     h('h2', 'User List'),
-                ].concat(state.users$.map(users =>
                     h('table', [
                         h('thead',[
                             h('tr', [
@@ -157,16 +140,15 @@ function main(responses){
                                 h('th', 'Delete')
                             ])
                         ]),
-                        h('tbody', users.map(user =>
-                            h('tr', [
-                                h('td', h('a.linkshowuser', {href:"#", rel:user.username}, user.username)),
-                                h('td', user.email),
-                                h('td', h('a.linkdeleteuser', {href:"#", rel:user._id}, 'delete'))
-                            ])
-                        )
-                    )
-                    ]) // table
-                ))), //div#userInfo
+                      h('tbody', state.user$.map( user =>
+                          h('tr', [
+                              h('td', h('a.linkshowuser', {href:"#", rel:user.username}, user.username)),
+                              h('td', user.email),
+                              h('td', h('a.linkdeleteuser', {href:"#", rel:user.username}, 'delete'))
+                          ])
+                        ))
+                      ]) // table
+                ]), // div#userList
                 h('div.addUser', [
                     h('h2', 'Add user'),
                     h('fieldset', [
@@ -186,19 +168,40 @@ function main(responses){
         ]) // div
     );
 
+    let getUser$ = Cycle.Rx.Observable.just("")
+        .map(() => {
+            return {
+                url: USERS_URL,
+                method: "GET"
+            }
+        });
+
+    let postNewUser = addNewUser$
+      .filter(u => u.username.length > 0 )
+      .map(u => {
+          return {
+            url: USER_ADD_URL,
+            method: "POST",
+            send: u
+          }
+      });
+    let deleteUser = removeUser$
+        .filter(id => id != null && id != undefined )
+        .map(id => USER_DELETE_URL+"/"+id)
+        .tap(x => console.log("tap deleteUser", x))
+        .map(id => {
+            return {
+              url: id,
+              method: "DEL"
+            }
+        });
+
     return{
         DOM: vtree$,
         HTTP: getUser$
-        .merge(addUserModel$
-            .filter(u => u.username.length > 0 )
-            .map(u => {
-                return {
-                    url: USERS_ADD_URL,
-                    method: "POST",
-                    send: u
-                }
-            }))
-        }
+        .merge(postNewUser)
+        .merge(deleteUser)
+    }
 }
 
 Cycle.run(main, {
